1d202b7b60becd0d208bbe023df230379c9e0436
AgustinXAlv1 <alvarezagustinx@gmail.com>
2022-07-04 23:25:19 -0300

    reestructuracion de las carpetas y subida del html de detalle de producto

--- prueba.js
+++ prueba.js
@@ -11,5 +11,5 @@ app.listen(3030, ()=>{
 })
 
 app.get('/', (req,res)=>{
-    res.sendFile(path.join(__dirname,"/views/descripcion.html"));
+    res.sendFile(path.join(__dirname,"/views/productDetail.html"));
 })
